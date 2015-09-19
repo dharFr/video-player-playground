5e1c77ade605c74cf8adc4a009866b599327e5c9
Olivier Audard <olivier.audard@dailymotion.com>
2015-09-19 16:12:38 +0200

    Revert playbackinfo to "before redux"

--- src/ui/playbackinfo.js
+++ src/ui/playbackinfo.js
@@ -2,7 +2,7 @@
 
 import flight         from 'flight';
 import withFormatTime from 'mixin/with_format_time';
-import withStore      from 'mixin/with_store';
+import withState      from 'with-state';
 import {
   VIDEO_DURATION_CHANGE,
   VIDEO_PAUSE,
@@ -12,22 +12,18 @@ import {
 
 function PlaybackInfo() {
 
-  this.shouldComponentUpdate = function(oldState, newState) {
-    return (
-      oldState.paused      !== newState.paused ||
-      oldState.duration    !== newState.duration ||
-      oldState.currentTime !== newState.currentTime
-    );
-  };
+  // Define an instance's `initialState`
+  this.initialState({
+    paused   : true,
+    time     : this.formatTime(0),
+    duration : this.formatTime(0)
+  });
 
   this.update = function() {
-    const currentTime = this.formatTime(this.state.currentTime);
-    const duration    = this.formatTime(this.state.duration);
-
     this.$node.html(`
       Playing: ${this.state.paused ? 'no' : 'yes'} <br>
-      Duration: ${duration}<br>
-      CurrentTime : ${currentTime}
+      Duration: ${this.state.duration}<br>
+      CurrentTime : ${this.state.time}
     `);
   };
 
@@ -35,6 +31,26 @@ function PlaybackInfo() {
 
     // Track changes to the state using advice
     this.after('stateChanged', this.update);
+
+    this.on('#root', VIDEO_PLAY, (e) => {
+      this.mergeState({ paused: false });
+    });
+
+    this.on('#root', VIDEO_PAUSE, (e) => {
+      this.mergeState({ paused: true });
+    });
+
+    this.on('#root', VIDEO_DURATION_CHANGE, (e, data) => {
+      this.mergeState({
+        duration: this.formatTime(data.duration)
+      });
+    });
+
+    this.on('#root', VIDEO_TIME_UPDATE, (e, data) => {
+      this.mergeState({
+        time: this.formatTime(data.time)
+      });
+    });
   });
 }
-export default flight.component(withFormatTime, withStore, PlaybackInfo);
+export default flight.component(withFormatTime, withState, PlaybackInfo);
